3ddd5367dab91d55647b963ac11e7484aa761f06
Santanu <ssaha1351@gmail.com>
2020-07-30 12:53:42 +0530

    Slight change in code

--- max_num.py
+++ max_num.py
@@ -4,7 +4,7 @@ num = int(input("Enter the total numbers to be checked in the list: "))
 for i in range(0,num):
     n = int(input("Enter the number: "))
     lst.append(n)
-print("The list: {}".format(lst))
+print("The list : {}".format(lst))
 
 # Finding the maximum number in the list
 print("The number with the maximum value in the list is: {}".format(max(lst)))
